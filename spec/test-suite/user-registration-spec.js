95616efaf211835d47c07ec5ec9e52687737bc9c
Jim Krehl <jimmyk@fnjurggen.localhost>
2023-03-07 01:05:14 +0000

    add test

--- spec/test-suite/user-registration-spec.js
+++ spec/test-suite/user-registration-spec.js
@@ -2,6 +2,8 @@ const axios = require ("axios");
 
 const base_url = "http://localhost:3000"
 
+const { userValidationFields } = require ('../../source/models/user.js');
+
 describe ("registration test suite", function () {
 	const end_point = base_url + '/api/user/register';
 
@@ -34,6 +36,64 @@ describe ("registration test suite", function () {
 					param_subset [p] = params [p];
 
 				await axios.post (end_point, param_subset)
+					.then (function (response) {
+						expect (true).toBe (false);
+					})
+					.catch (function (error) {
+						expect (error.response.status).toBe (400);
+					});
+			}
+		});
+	});
+
+	describe ("POST /api/user/register", function () {
+		const min_params = {
+			screen_name: "a",
+			email: "a@b.c",
+			password: "a" };
+
+		const max_length = (key) =>
+			userValidationFields [key]._rules.filter (r => r.name == 'max') [0].args.limit;
+
+		const max_params = {
+			screen_name: "a".repeat (max_length ('screen_name') + 1),
+			email: "a".repeat (max_length ('email') + 1) + "@mail.com",
+			password: "a".repeat (max_length ('password') + 1) };
+
+		const valid_params = {
+			screen_name: "test_user",
+			email: "test_user@mail.com",
+			password: "password" };
+
+		const test_params = [
+			{
+				screen_name: min_params.screen_name,
+				email: valid_params.email,
+				password: valid_params.password },
+			{
+				screen_name: max_params.screen_name,
+				email: valid_params.email,
+				password: valid_params.password },
+			{
+				screen_name: valid_params.screen_name,
+				email: min_params.email,
+				password: valid_params.password },
+			{
+				screen_name: valid_params.screen_name,
+				email: max_params.email,
+				password: valid_params.password },
+			{
+				screen_name: valid_params.screen_name,
+				email: valid_params.email,
+				password: min_params.password },
+			{
+				screen_name: valid_params.screen_name,
+				email: valid_params.email,
+				password: max_params.password } ];
+
+		it ("invalid parameter lengths", async function () {
+			for (params of test_params) {
+				await axios.post (end_point, params)
 					.then (function (response) {
 						expect (true).toBe (false);
 					})

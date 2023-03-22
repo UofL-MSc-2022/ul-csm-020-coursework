f8de7a12246fb61b5c04ffdbc0ff8a85ec6d8792
Jim Krehl <jimmyk@fnjurggen.localhost>
2023-03-22 23:01:14 +0000

    prevent default empty arrays in PostModel for comments and likes

--- source/models/post.js
+++ source/models/post.js
@@ -5,8 +5,14 @@ const postSchema = mongoose.Schema ({
 	title: { type: String, required: true, min: 3, max: 256 },
 	body: { type: String, required: true, min: 3, max: 1024 },
 	owner: { type: mongoose.Schema.Types.ObjectId, ref: 'UserModel', required: true },
-	comments: [ { type: mongoose.Schema.Types.ObjectId, ref: 'CommentModel', select: false }],
-	likes: [ { type: mongoose.Schema.Types.ObjectId, ref: 'LikeModel', select: false }],
+	comments: {
+		type: [{type: mongoose.Schema.Types.ObjectId, ref: 'CommentModel'}],
+		default: undefined,
+		select: false },
+	likes: {
+		type: [{type: mongoose.Schema.Types.ObjectId, ref: 'LikeModel'}],
+		default: undefined,
+		select: false },
 	n_likes: { type: Number, select: false }
 }, {timestamps: true});
 

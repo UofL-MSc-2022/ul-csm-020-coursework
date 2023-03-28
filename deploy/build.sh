95ea7df98fbc59c56c396fa0d3073a570b081d54
Jim Krehl <you@example.com>
2023-03-28 23:00:05 +0000

    delete image before building

--- deploy/build.sh
+++ deploy/build.sh
@@ -9,10 +9,13 @@ echo -n "  create $NETWORK_NAME: "
 docker network create --subnet=$IP_RANGE --ip-range=$IP_RANGE --gateway=$GATEWAY $NETWORK_NAME
 
 echo "Build prod-app ..."
+docker image rm $PROD_APP_IMAGE
 docker build -t $PROD_APP_IMAGE --target prod-app .
 
 echo "Build test-app ..."
+docker image rm $TEST_APP_IMAGE
 docker build -t $TEST_APP_IMAGE --target test-app .
 
 echo "Build test-suite ..."
+docker image rm $TEST_SUITE_IMAGE
 docker build -t $TEST_SUITE_IMAGE --target test-suite .

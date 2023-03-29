8631753b5fe028080a671377b4074047732dae75
Jim Krehl <jimmyk@fnjurggen.localhost>
2023-03-29 12:44:11 +0100

    switch to curl for requests, add test case #4

---
+++ test-cases/tc-4.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/zsh
+
+source ${0:a:h}/setup
+source ${0:a:h}/tokens
+
+end_point="/api/post/create"
+post_data='{"title":"Olga Title","body":"Olga body."}'
+
+echo "Request:"
+echo "\tPOST $end_point"
+
+post_request $end_point $post_data $OLGA_TOKEN
+
+echo "Response:"
+echo "\tHTTP status code: $post_code"
+echo "\tServer response body: $post_body"

9db1acf5663c37751bb463c59cc8272b1a952068
Jim Krehl <jimmyk@fnjurggen.localhost>
2023-03-29 13:58:44 +0100

    update output

--- test-cases/tc-7.sh
+++ test-cases/tc-7.sh
@@ -6,12 +6,14 @@ source ${0:a:h}/tokens
 end_point="/api/post/list/all"
 
 for token in $NICK_TOKEN $OLGA_TOKEN ; do
-	echo "Request:"
-	echo "\tGET $end_point"
+	echo "--- Request ------------"
+	echo "GET $end_point"
 
 	get_request $end_point $token
 
-	echo "Response:"
-	echo "\tHTTP status code: $get_code"
-	echo "\tServer response body: $get_body"
+	echo "--- Response -----------"
+	echo $get_code
+	echo $get_body | $pretty_print_json[@]
+	echo "------------------------"
+	echo
 done

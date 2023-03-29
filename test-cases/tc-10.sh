e320df04bb1f03de0b8e6c3a09eebb194e91046b
Jim Krehl <jimmyk@fnjurggen.localhost>
2023-03-29 17:12:55 +0100

    add test case #10

---
+++ test-cases/tc-10.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/zsh
+
+source ${0:a:h}/setup
+source ${0:a:h}/tokens
+
+end_point="/api/post/list/all"
+
+echo "--- Request ------------"
+echo "GET $end_point"
+
+get_request $end_point $MARY_TOKEN
+
+echo "--- Response -----------"
+echo $get_code
+echo $get_body | $pretty_print_json[@]
+echo "------------------------"

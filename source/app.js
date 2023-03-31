34b212fefc30dce89ded0f229828488fc703a688
Jim Krehl <jimmyk@fnjurggen.localhost>
2023-03-31 12:33:05 +0100

    add comments

--- source/app.js
+++ source/app.js
@@ -7,27 +7,33 @@ require ('dotenv/config');
 
 const app = express ();
 
+// Add middleware to marshal JSON strings in HTTP requests into JS objects
 app.use (bodyParser.json ());
 
+// Load route objects
 const versionRoute = require ('./routes/version');
 const userRoute = require ('./routes/user');
 const postRoute = require ('./routes/post');
 const commentRoute = require ('./routes/comment');
 const likeRoute = require ('./routes/like');
 
+// Add routes
 app.use ('/api/version', versionRoute);
 app.use ('/api/user', userRoute);
 app.use ('/api/post', postRoute);
 app.use ('/api/comment', commentRoute);
 app.use ('/api/like', likeRoute);
 
-const mongo_db_url = new URL (process.env.DB_URL);
+// Create URL object from URL string in order to extract the database name
+const mongoDBURL = new URL (process.env.DB_URL);
 
+// Connect to MongoDB
 mongoose.set ('strictQuery', true);
-mongoose.connect (mongo_db_url.href, () => {
-	console.log ('MongoDB connected [' + mongo_db_url.pathname + '] ...');
+mongoose.connect (mongoDBURL.href, () => {
+	console.log ('MongoDB connected [' + mongoDBURL.pathname + '] ...');
 });
 
+// Open application up for requests
 app.listen (process.env.APP_PORT, process.env.APP_HOST, () => {
 	console.log ('Server is listening on ' + process.env.APP_HOST + ':' + process.env.APP_PORT + ' ...');
 });

3474bcec5a851dce193faff5e3d43445ca2c096d
Jim Krehl <jimmyk@fnjurggen.localhost>
2023-03-31 12:43:56 +0100

    clean up code

--- source/routes/version.js
+++ source/routes/version.js
@@ -1,12 +1,12 @@
 const express = require ('express');
-const package_config = require ('../../package.json');
+const packageJSON = require ('../../package.json');
 
-const { jwtAuth } = require ('../auth/jwt');
+const {jwtAuth} = require ('../auth/jwt');
 
 const router = express.Router ();
 
-router.get ('/', jwtAuth, async (req, res) => {
-	res.send ({version: package_config.version});
+router.get ('/', jwtAuth, (req, res) => {
+	res.send ({version: packageJSON.version});
 });
 
 module.exports = router;

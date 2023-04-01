93d7c35c0ca658cdb25150c853e1be4f3e8a178c
Jim Krehl <jimmyk@fnjurggen.localhost>
2023-04-01 03:55:36 +0100

    add comment

--- spec/support/verbose-reporter.js
+++ spec/support/verbose-reporter.js
@@ -1,3 +1,5 @@
+// Add extra logging to the test run output, the number of expect() calls per
+// spec.
 const VerboseReporter = {
 	specDone: function (result) {
 		console.log (result.description + ': ' + result.passedExpectations.length);

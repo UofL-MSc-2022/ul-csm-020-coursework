031e6f2054e1338bf0a6de1adda5bb81b46969f8
Jim Krehl <jimmyk@fnjurggen.localhost>
2023-04-01 14:51:50 +0100

    switch to camel case

--- spec/test-suite/auth-spec.js
+++ spec/test-suite/auth-spec.js
@@ -8,38 +8,45 @@ const {createAccessToken, verifyAccessToken} = require ('../../source/auth/jwt')
 
 common.initTestSuite ();
 
+// All axios requests include 'expect (true).toBe (false);' in one of the
+// callbacks.  For example, if a request is expected to succeed, the error call
+// back contains the above expect().  If a request fails when it is expected to
+// succeed the expect() calls in the success call back won't be called and they
+// won't appear as a failure to Jasmine.  The above expect() is guaranteed to
+// fail, this ensures that the spec fails when it should.
+
 describe ("JWT auth tests:", function () {
 	// Sample endpoint for verifying auth.  It must be an endpoint that
 	// requires authorisation.
-	const sample_endpoint = common.BASE_URL + '/api/post/list/all';
+	const sampleEndpoint = common.BASE_URL + '/api/post/list/all';
 
 	beforeAll (common.connectToTestDB);
-	beforeEach (async function () { this.test_users = await common.reloadTestUsers (); });
+	beforeEach (async function () { this.testUsers = await common.reloadTestUsers (); });
 
 	// Verify that the payload created by sign-in contains the user id.
 	it ("Payload must contain correct information.", async function () {
-		const sign_in_endpoint = common.BASE_URL + '/api/user/sign-in';
+		const signInEndpoint = common.BASE_URL + '/api/user/sign-in';
 
-		for (const user of this.test_users) {
+		for (const user of this.testUsers) {
 			const params = {
 				email: user.email,
-				password: user.password_plain };
+				password: user.passwordPlain };
 
-			await axios.post (sign_in_endpoint, params)
+			await axios.post (signInEndpoint, params)
 				.then (async function (res) {
-					token_subject = verifyAccessToken (res.data['auth-token']).sub;
+					const tokenSubject = verifyAccessToken (res.data['auth-token']).sub;
 
-					expect (token_subject).toBe (user.id);
+					expect (tokenSubject).toBe (user.id);
 				})
 		}
 	});
 
 	// Create a token and verify that it grants access to the sample endpoint.
 	it ("Valid token grants access.", async function () {
-		for (const user of this.test_users) {
+		for (const user of this.testUsers) {
 			const header = {headers: common.createTokenHeader (user.id)};
 
-			await axios.get (sample_endpoint, header)
+			await axios.get (sampleEndpoint, header)
 				.then (function (res) {
 					expect (res.status).toBe (200);
 				})
@@ -51,7 +58,7 @@ describe ("JWT auth tests:", function () {
 
 	// Verify that requests with an Authorization header are rejected.
 	it ("Token header is required.", async function () {
-		await axios.get (sample_endpoint)
+		await axios.get (sampleEndpoint)
 			.then (function (res) {
 				expect (true).toBe (false);
 			})
@@ -64,7 +71,7 @@ describe ("JWT auth tests:", function () {
 	it ("Token cannot be malformed.", async function () {
 		const header = {headers: {Authorization: "Bearer DEADBEEF"}};
 
-		await axios.get (sample_endpoint, header)
+		await axios.get (sampleEndpoint, header)
 			.then (function (res) {
 				expect (true).toBe (false);
 			})
@@ -77,10 +84,10 @@ describe ("JWT auth tests:", function () {
 	// database.  Fradulent ids that are not 24 digit hexidecimal numbers will
 	// be rejected before attempting to load a user from the database.
 	it ("Payload must contain a valid user id.", async function () {
-		const valid_token = createAccessToken ('12345678DEADBEEF98765432');
-		const header = {headers: {Authorization: "Bearer " + valid_token}};
+		const validToken = createAccessToken ('12345678DEADBEEF98765432');
+		const header = {headers: {Authorization: "Bearer " + validToken}};
 
-		await axios.get (sample_endpoint, header)
+		await axios.get (sampleEndpoint, header)
 			.then (function (res) {
 				expect (true).toBe (false);
 			})
@@ -91,17 +98,17 @@ describe ("JWT auth tests:", function () {
 
 	// Verify that token expiration works.
 	it ("Token cannot expire.", async function () {
-		for (const user of this.test_users) {
+		for (const user of this.testUsers) {
 			// Use ms to convert time strings, e.g. '20m', to integer
 			// milliseconds.
-			const token_expiry_ms = ms (config.get ('jwt_access_token_expiry'));
+			const tokenExpiryMS = ms (config.get ('jwt_access_token_expiry'));
 			// Create a mock issue time 1 second longer than the configured
 			// expiry.
-			const mock_issue = new Date (Date.now () - (token_expiry_ms + 1000));
+			const mockIssue = new Date (Date.now () - (tokenExpiryMS + 1000));
 
-			// Fake the system time to the mock_issue time.
+			// Fake the system time to the mockIssue time.
 			jasmine.clock ().install ();
-			jasmine.clock ().mockDate (mock_issue);
+			jasmine.clock ().mockDate (mockIssue);
 
 			// Create the token within the context of the faked system time.
 			const token = createAccessToken (user.id);
@@ -111,7 +118,7 @@ describe ("JWT auth tests:", function () {
 
 			const header = {headers: {Authorization: "Bearer " + token}};
 
-			await axios.get (sample_endpoint, header)
+			await axios.get (sampleEndpoint, header)
 				.then (function (res) {
 					expect (true).toBe (false);
 				})

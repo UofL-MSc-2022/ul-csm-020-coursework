f74e518eb26e35799cafc04c52570049f26ca872
Jim Krehl <jimmyk@fnjurggen.localhost>
2023-04-02 03:14:23 +0100

    start swagger integration

--- source/app.js
+++ source/app.js
@@ -3,7 +3,10 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
 const bodyParser = require ('body-parser');
+const swagger = require ('swagger-ui-express');
 require ('dotenv/config');
+
+const swaggerConfig = require ('../config/swagger.json');
 
 const app = express ();
 
@@ -20,6 +23,7 @@ const likeRoute = require ('./routes/like');
 
 // Add routes
 app.use ('/api/version', versionRoute);
+app.use ('/api/doc', swagger.serve, swagger.setup (swaggerConfig));
 app.use ('/api/user', userRoute);
 app.use ('/api/post', postRoute);
 app.use ('/api/comment', commentRoute);
